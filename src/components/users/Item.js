56cc561a21a26ca274c7d9dab7e003cb69f677d5
pearlpandz <pearlpandzz@gmail.com>
2020-01-07 09:00:53 +0530

    user item card converted to class to funciton based

--- src/components/users/Item.js
+++ src/components/users/Item.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Item.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-class UserItem extends Component {
- 
-  render() {
-    const { login, avatar_url } = this.props.user;
-
-    return (
-      <div className="card">
-        <img src={avatar_url} alt="" />
-        <h4>{login.toUpperCase()}</h4>
-        <Link to={`/user/${login}`}>more</Link>
-      </div>
-      );
-  }
+const UserItem = (props) => {
+  const { login, avatar_url } = props.user;
+  return (
+    <div className="card">
+      <img src={avatar_url} alt="" />
+      <h4>{login.toUpperCase()}</h4>
+      <Link to={`/user/${login}`}>more</Link>
+    </div>
+  );
 }
 
 
